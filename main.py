9ecb30ef106ac9008d9abf0e50bad62a60cdb861
szymonczternasty <sczternasty@gmail.com>
2023-11-21 20:33:57 +0100

    New functionality added, scraping tiles information and adding them to pandas dataframe

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 res = requests.get('https://kamernet.nl/en/for-rent/rooms-amsterdam?radius=5&minSize=&maxRent=')
-x = BeautifulSoup(res.text, 'html.parser')
+soup = BeautifulSoup(res.text, 'html.parser')
+data = soup.select('.tile-data')
+tab = []
+for element in data:
+    partial_data = {}
+    partial_data["Title"] = element.find('a').getText().strip()
+    partial_data["Location"] = element.select('.tile-city')[0].getText().strip()
+    partial_data["Type"] = element.select(('.tile-room-type'))[0].getText().strip().split(" ")[-1]
 
-print(x)
+
+    tab.append(partial_data)
+
+df = pd.DataFrame(tab)
+print(df)
