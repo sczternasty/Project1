4ff14652c69f60bd90918dfd4ff729737627a2fc
szymonczternasty <sczternasty@gmail.com>
2023-11-22 18:17:07 +0100

    New information regarding offerings are scraped (price, utilities, surface, furnished and availability)

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ for element in data:
     partial_data = {}
     partial_data["Title"] = element.find('a').getText().strip()
     partial_data["Location"] = element.select('.tile-city')[0].getText().strip()
-    partial_data["Type"] = element.select(('.tile-room-type'))[0].getText().strip().split(" ")[-1]
-
-
+    partial_data["Type"] = element.select('.tile-room-type')[0].getText().strip().split(" ")[-1]
+    partial_data["Price"] = element.select('.tile-rent')[0].getText().strip().split(" ")[1].split(",")[0]
+    partial_data["Utilities_included"] = True if len(element.select('.tile-rent')[0].select('span')[0].getText().strip().split(" ")) > 1 else False
+    partial_data["Surface"] = element.select('.tile-surface')[0].getText().strip().split(" ")[0]
+    partial_data["Furnished"] = element.select('.tile-furnished')[0].getText().strip().split(" ")[0]
+    partial_data["Availability_start_date"]= element.select('.tile-availability')[0].getText().strip().split(" ")[0]
+    partial_data["Availability_end_date"] = element.select('.tile-availability')[0].getText().strip().split(" ")[1].split("\n")[-1]
     tab.append(partial_data)
 
 df = pd.DataFrame(tab)
